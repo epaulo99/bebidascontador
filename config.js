2310bf97418f6aa3c24a4131d906c26103ebac6d
epaulo99 <eliabe.paulo99@gmail.com>
2026-04-02 15:56:25 -0300

    chore: versionar config.js para deploy na Vercel

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://ypkezqkloqdsdcfczjdc.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwa2V6cWtsb3Fkc2RjZmN6amRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzMzMDEsImV4cCI6MjA5MDcwOTMwMX0.sxbkuowHP_0csh-26ioLQqUf284yqc3g6FKMLgOD1G0"
+};
